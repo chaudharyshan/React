9b135144cd6aa84c8b8f70b6cd6a258d40ba5bf9
chaudharyshan <chaudharyshan1111@gmail.com>
2024-01-30 16:29:38 +0530

    button

--- 01basicreact/src/shan.jsx
+++ 01basicreact/src/shan.jsx
@@ -1,7 +1,17 @@
+import * as React from 'react';
+
 function Shan() {
  return(
- <h3>shan is ready</h3>
+    <>
+     <h3>shan is ready</h3>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore temporibus suscipit accusantium repellendus dolorum beatae amet, 
+        corporis quia hic facilis molestias sapiente fuga obcaecati et ab accusamus magni magnam. Unde!
+     </p>
+        
+    </>
+
  )
+
     
  
 }
